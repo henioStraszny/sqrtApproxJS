35127502f60b5cf946e0849a69727008b3e8cddc
henioStraszny <jacek.styrylski@gmail.com>
2016-10-16 12:43:04 +0200

    Initial repo for GO

---
+++ sqrtApproxJS.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+//  Sqrt x
+func Sqrt(x float64) float64 {
+	return 0.0
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println("Pierwszy program Go Jacka")
+}
